046f7f99c86e48f4bc5341278def91c9c71dc71d
Kamil Macek <milo.macek@gmail.com>
2020-05-15 12:47:53 +0200

    SK cities endpoint

--- src/main/java/sk/pis/spodBEH_server/controller/RunController.java
+++ src/main/java/sk/pis/spodBEH_server/controller/RunController.java
@@ -1,5 +1,6 @@
 package sk.pis.spodBEH_server.controller;
 
+import mypackage.Cities;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import sk.pis.spodBEH_server.model.Run;
 import sk.pis.spodBEH_server.model.RunRunner;
 import sk.pis.spodBEH_server.repo.RunRepository;
 import sk.pis.spodBEH_server.repo.RunRunnerRepository;
+import sk.pis.spodBEH_server.service.CityService;
 import sk.pis.spodBEH_server.service.NotificationService;
 
 import javax.transaction.Transactional;
@@ -27,6 +29,9 @@ public class RunController {
     @Autowired
     NotificationService notificationService;
 
+    @Autowired
+    CityService cityService;
+
     @GetMapping(value = "/getAll", produces = "application/json")
     public List<Run> getAll() {
         return runRepository.findAll();
@@ -43,5 +48,10 @@ public class RunController {
     public void unregister(@PathVariable Long runId, @PathVariable Long runnerId) {
         runRunnerRepository.deleteRunRunnerByRunIdAndRunnerId(runId, runnerId);
         notificationService.sendSmsNotificationRunRegistration(runId, runnerId, false);
+    }
+
+    @GetMapping(value = "/getSKCities", produces = "application/json")
+    public List<Cities> getAllSKCities() {
+        return cityService.getSKCities();
     }
 }
